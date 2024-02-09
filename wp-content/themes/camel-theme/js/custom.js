015c92bede3ed0dd1b14fe6a1c8565e1b4b1569f
IagoGoncalves <iago_375@hotmail.com>
2024-02-09 14:16:37 -0300

    ajustes

--- wp-content/themes/camel-theme/js/custom.js
+++ wp-content/themes/camel-theme/js/custom.js
@@ -6,27 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileMenuToggle.classList.toggle("active-menu");
         hamburger.classList.toggle("cross-menu");
     });
-
-    var swiper = new Swiper('.banner-home', {
-        speed: 5000,
-        loop: true,
-        spaceBetween: 30,
-        effect: 'fade',
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-            renderBullet: function (index, className) {
-              return '<span class="' + className + '">' + (index + 1) + "</span>";
-            },
-          },
-        // autoplay: {
-        //     delay: 2500,
-        // },    
-    });
+    
     var swiper = new Swiper('.galery-per-view', {
         speed: 5000,
         slidesPerView: 3,
@@ -47,11 +27,50 @@ document.addEventListener('DOMContentLoaded', function () {
         //     delay: 2500,
         // },    
     });
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 200) {
+    
+    var locale = window.location.pathname;
+    if (locale === '/') {
+      window.addEventListener('scroll', function () {
+        if (window.scrollY > 25) {
             document.getElementById("header-menu").classList.add("internal-header");
         } else {
             document.getElementById("header-menu").classList.remove("internal-header");
         }
-    });
+      });
+
+      var swiper = new Swiper('.banner-home', {
+        speed: 5000,
+        loop: true,
+        spaceBetween: 30,
+        effect: 'fade',
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+          },
+        // autoplay: {
+        //     delay: 2500,
+        // },    
+      });
+
+      var tagLi = document.querySelector('li.classe-a-ser-adicionada');
+      if (tagLi) {
+          tagLi.classList.add('nova-classe');
+      }
+    }
+
+    document.querySelectorAll('.menu > li').forEach(function(li) {
+      if (li.classList.contains('current-menu-item')) {
+          var link = li.querySelector('a');
+          if (link) {
+              link.classList.add('menu-li-active');
+          }
+      }
+  });
 });
--- wp-content/themes/camel-theme/parts/internal-breadcrump.php
+++ wp-content/themes/camel-theme/parts/internal-breadcrump.php
@@ -1,8 +1,8 @@
 <section class="breadcrump">
     <h1 class="h2-bold-title"><?php the_title(); ?></h1>
     <span class="align">
-        <a href="" class="h3-light">Home ></a>
-        <a href="" class="h3-light">Serviços</a>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="h3-light">Home ></a>
+        <a href="<?php echo esc_url( home_url( '/servicos' ) ); ?>" class="h3-light">Serviços</a>
         <p class="h3-light"> > <?php the_title(); ?></p>
     </span>
 </section>
